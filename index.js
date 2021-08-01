837f05efe433d5d0fcc2569abfd3b5c01753b1f4
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-01 00:44:30 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+const names=['Ada', 'Brendan', 'Ali'];
+const event='birthday';
+
+function writeCards(names, event) {
+    let emptyArray=[];
+    for (let n = 0; n < names.length; n++) {
+       emptyArray.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`);
+}
+    return emptyArray;
+}
+
+function countDown(integer) {
+    while (integer >=0) {
+        console.log(integer--);
+    }
+    return countDown;
+}
+    
